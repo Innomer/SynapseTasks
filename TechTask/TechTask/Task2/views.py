771860bc3da4c69567f7574589631c1cc9c19af2
Innomer <innomershyper@gmail.com>
2022-09-04 17:51:22 +0530

    Task 2 Complete

--- TechTask/TechTask/Task2/views.py
+++ TechTask/TechTask/Task2/views.py
@@ -38,7 +38,33 @@ def UpdateEvent(request):
     choice=request.POST.get("CRUDchoice")
     if(choice):
         return redirect('/task2/{}'.format(choice))
-    return render(request,"eve.html")
+    eventList=list()
+    for e in EventDet.objects.all():
+        eventList.append(model_to_dict(e))
+    delIndex=request.POST.get("UpdChoice")
+    if(delIndex):
+        for ind,n in enumerate(eventList):
+            if(ind==int(delIndex)):
+                e=EventDet.objects.filter(name=n['name'])
+                for ev in e:
+                    name=request.POST.get("name")
+                    date=request.POST.get("date")
+                    desc=request.POST.get("des")
+                    completedEvent=request.POST.get("com")
+                    print(name,date,desc,completedEvent)
+                    if(name):
+                        ev.name=name
+                    if(date):
+                        ev.date=date
+                    if(desc):
+                        ev.description=desc
+                    if(completedEvent=="on"):
+                        ev.complete=True
+                    else:
+                        ev.complete=False
+                    ev.save()
+        return render(request,"eve.html",{'update':1,'eventList':enumerate(eventList),'updated':1})    
+    return render(request,"eve.html",{'update':1,'eventList':enumerate(eventList)})
 
 def ViewEvents(request):
     choice=request.POST.get("CRUDchoice")

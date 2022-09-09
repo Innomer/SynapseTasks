529b0564fcc84fd9f38b2034505729ed331277b9
Innomer <innomershyper@gmail.com>
2022-09-09 14:07:57 +0530

    Minor Changes

--- Task1/t1_3.py
+++ Task1/t1_3.py
@@ -6,9 +6,10 @@ class Titandex:
         self.streak=0
     
     def TitanvsScout(self,scoutname,scoutstrength):
+        print("{} vs {}".format(self.name,scoutname))
         if(self.strength>scoutstrength):
             self.streak+=1
-            print("Titan {} with strength {} and height {}m has won".format(self.name,self.strength,self.height))
+            print("{} with strength {} and height {}m has won. \n Winning Streak is {}".format(self.name,self.strength,self.height,self.streak))
         elif(self.strength==scoutstrength):
             print("It is a draw!")
             self.streak=0
@@ -16,26 +17,35 @@ class Titandex:
             print("Scout {} with strength {} has won".format(scoutname,scoutstrength))
             self.streak=0
 
-    def TitanvsTitan(self,titanname,titanheight,titanstrength):
+    def TitanvsTitan(self,titan):
+        print("{} vs {}".format(self.name,titan.name))
 
-        if(self.name==titanname):
+        if(self.name==titan.name):
             print("You Cannot Make The Titan Fight Itself")
         else:
-            if(self.strength>titanstrength):
-                print("Titan {} with strength {} and height {}m has won".format(self.name,self.strength,self.height))
+            if(self.strength>titan.strength):
                 self.streak+=1
-            elif(self.strength==titanstrength):
+                titan.streak=0
+                print("{} with strength {} and height {}m has won. \n Winning Streak of {} is {}".format(self.name,self.strength,self.height,self.name,self.streak))
+            elif(self.strength==titan.strength):
                 print("Its a Draw")
                 self.streak=0
+                titan.streak=0
             else:
-                print("Titan {} with strength {} and height {}m has won".format(titanname,titanstrength,titanheight))
                 self.streak=0
+                titan.streak+=1
+                print("{} with strength {} and height {}m has won. \n Winning Streak of {} is {}".format(titan.name,titan.strength,titan.height,titan.name,titan.streak))
 
 bT=Titandex("Beast Titan",17,250)
+arT=Titandex("Armoured Titan",15,250)
+fT=Titandex("Founding Titan",13,350)
+cT=Titandex("Cart Titan",4,175)
+
 bT.TitanvsScout("Levi",300)
 bT.TitanvsScout("Armin",250)
 bT.TitanvsScout("Sasha",200)
-bT.TitanvsTitan("Founding Titan",13,350)
-bT.TitanvsTitan("Armored Titan",15,250)
-bT.TitanvsTitan("Cart Titan",4,175)
-bT.TitanvsTitan("Beast Titan",17,250)
+
+bT.TitanvsTitan(fT)
+bT.TitanvsTitan(arT)
+bT.TitanvsTitan(cT)
+bT.TitanvsTitan(bT)

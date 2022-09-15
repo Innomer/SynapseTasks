159fcd3e0e7f741f09dc6b77b55cf0cfd3a7fef3
Innomer <innomershyper@gmail.com>
2022-09-15 20:25:01 +0530

    DELETED

--- TechTask/TechTask/Task1/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns=[
-    path('allDogs',views.dog),
-    path('subDogs/<str:breed>',views.subdog),
-    path('random/',views.randomBreed),
-]
